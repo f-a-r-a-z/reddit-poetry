039fffa2a449ae24fda0df3dc465d2bd21224c92
Faraz <farazshafiq99+git@gmail.com>
2019-07-14 13:25:29 +1000

    Updates

--- precache-manifest.ebb901f9e57760ff9274bc2c0a143a8c.js
+++ precache-manifest.ebb901f9e57760ff9274bc2c0a143a8c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "05e25492a054e3576080ea63427986c1",
+    "revision": "98ca7107908140ae7df7d58f24f0ab9e",
     "url": "/reddit-poetry/index.html"
   },
   {
-    "revision": "64ffb22c25e75528aec7",
-    "url": "/reddit-poetry/static/css/main.2ab59a3f.chunk.css"
+    "revision": "6ff9c00a70d45e76e2b8",
+    "url": "/reddit-poetry/static/css/main.2e2f6d59.chunk.css"
   },
   {
     "revision": "7d3b42f06656cf2adc2e",
     "url": "/reddit-poetry/static/js/2.a9ca1a38.chunk.js"
   },
   {
-    "revision": "64ffb22c25e75528aec7",
-    "url": "/reddit-poetry/static/js/main.ba67345b.chunk.js"
+    "revision": "6ff9c00a70d45e76e2b8",
+    "url": "/reddit-poetry/static/js/main.ef80c2cb.chunk.js"
   },
   {
     "revision": "15e675e5c73bb06162d3",
